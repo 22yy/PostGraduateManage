48b15989c52670fb4ef70175389a4eef3b6977ab
asyouwish22 <2061351540@qq.com>
2023-05-09 09:13:42 +0800

    1

---
+++ src/components/r-header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+     <el-menu class="el-menu" mode="horizontal">
+     <el-menu-item index="1" @click=out()>logout</el-menu-item>
+     <el-menu-item index="2">
+      <i class="el-icon-paperclip" style="margin-right: 12px"></i>
+      <span>{{ currentTerm }}</span>
+     </el-menu-item>
+     <el-menu-item index="3">
+      <i class="el-icon-user" style="margin-right: 12px"></i>
+      <span>{{ name }}</span>
+     </el-menu-item>
+     </el-menu>
+      <!-- <el-button type="text" style="color: #333333; font-size: 18px" @click="out()">
+        logout
+      </el-button>
+      <el-divider direction="vertical" style="color: #333333"></el-divider>
+      <i class="el-icon-paperclip" style="margin-right: 18px"></i>
+      <span>{{ currentTerm }}</span>
+      <el-divider direction="vertical"></el-divider>
+      <i class="el-icon-user" style="margin-right: 18px"></i>
+      <span>{{ name }}</span> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "r-header",
+  data() {
+    return {
+      name: null,
+      currentTerm: null
+    }
+  },
+  created() {
+    this.name = sessionStorage.getItem("name")
+    this.currentTerm = sessionStorage.getItem("currentTerm")
+  },
+  methods: {
+    out() {
+      sessionStorage.clear();
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  position: absolute;
+  width: 40%;
+  right: 0;
+}
+.el-menu {
+   background-color: #eee;
+}
+.el-menu--horizontal>.el-menu-item.is-active {
+  border: none;
+}
+</style>
---
+++ src/views/Admin/teacherManage/editorTeacher.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+      <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/" style="margin-bottom:20px">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>教师管理</el-breadcrumb-item>
+      <el-breadcrumb-item>编辑教师</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-form style="width: 60%" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="教师姓名" prop="tname">
+        <el-input v-model="ruleForm.tname" :value="ruleForm.tname"></el-input>
+      </el-form-item>
+      <el-form-item label="初始密码" prop="password">
+        <el-input v-model="ruleForm.password" :value="ruleForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <!-- <el-button @click="test">test</el-button> -->
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        tid: null,
+        tname: null,
+        password: null
+      },
+      rules: {
+        tname: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 2, max: 5, message: '长度在 2 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'change' }
+        ],
+      }
+    };
+  },
+  created() {
+    const that = this
+    if (this.$route.query.tid === undefined) {
+      this.ruleForm.tid = 6
+    }
+    else {
+      this.ruleForm.tid = this.$route.query.tid
+    }
+    axios.get('http://175.24.188.37:10086/teacher/findById/' + this.ruleForm.tid).then(function (resp) {
+      that.ruleForm = resp.data
+    })
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // 通过前端校验
+          const that = this
+          if (that.ruleForm.tname === 'admin') {
+            that.$message({
+              showClose: true,
+              message: 'admin 不可编辑',
+              type: 'error'
+            });
+            this.$router.push('/queryTeacher')
+            return
+          }
+          console.log(this.ruleForm)
+          axios.post("http://175.24.188.37:10086/teacher/updateTeacher", this.ruleForm).then(function (resp) {
+            if (resp.data === true) {
+              that.$message({
+                showClose: true,
+                message: '编辑成功',
+                type: 'success'
+              });
+            }
+            else {
+              that.$message.error('编辑失败，请检查数据库');
+            }
+            that.$router.push("/queryTeacher")
+          })
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    test() {
+      console.log(this.ruleForm)
+    }
+  }
+}
+</script>
